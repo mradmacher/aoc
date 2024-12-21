421d869016001241c2d93be94c36988d8b818c41
mradmacher <michal@radmacher.pl>
2024-12-22 00:31:12 +0100

    add the second part solution

--- src/main.zig
+++ src/main.zig
@@ -18,9 +18,13 @@ fn loadLists(left: *std.ArrayList(u32), right: *std.ArrayList(u32), path: []cons
     }
 }
 
-fn computeDistance(left_items: []u32, right_items: []u32) u32 {
-    std.mem.sort(u32, left_items, {}, comptime std.sort.asc(u32));
-    std.mem.sort(u32, right_items, {}, comptime std.sort.asc(u32));
+fn sort(slice: []u32) void {
+    std.mem.sort(u32, slice, {}, comptime std.sort.asc(u32));
+}
+
+fn computeDistance(left_items: []u32, right_items: []u32) !u32 {
+    sort(left_items);
+    sort(right_items);
 
     const size = left_items.len;
     var sum: u32 = 0;
@@ -31,9 +35,36 @@ fn computeDistance(left_items: []u32, right_items: []u32) u32 {
     return sum;
 }
 
+fn countOccurrences(occurrences: *std.AutoHashMap(u32, u32), slice: []u32) !void {
+    for (slice) |value| {
+        const found = try occurrences.getOrPut(value);
+        //std.debug.print("{d}: {any}\n", .{value, found});
+        if (found.found_existing) {
+            found.value_ptr.* += 1;
+        } else {
+            found.value_ptr.* = 1;
+        }
+    }
+}
+
+fn computeSimilarity(items: []u32, occurrences: *std.AutoHashMap(u32, u32)) u32 {
+    var sum: u32 = 0;
+
+    for (items) |value| {
+        const found = occurrences.getEntry(value) orelse continue;
+        sum += value * found.value_ptr.*;
+    }
+
+    return sum;
+}
+
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const allocator = gpa.allocator();
+
+    var occurrences = std.AutoHashMap(u32, u32).init(allocator);
+    defer occurrences.deinit();
 
     var left_list = std.ArrayList(u32).init(allocator);
     var right_list = std.ArrayList(u32).init(allocator);
@@ -42,7 +73,10 @@ pub fn main() !void {
 
     try loadLists(&left_list, &right_list, "./input");
 
-    std.debug.print("Sum: {d}\n", .{computeDistance(left_list.items, right_list.items)});
+    std.debug.print("Distance: {d}\n", .{try computeDistance(left_list.items, right_list.items)});
+
+    try countOccurrences(&occurrences, right_list.items);
+    std.debug.print("Similarity: {d}\n", .{computeSimilarity(left_list.items, &occurrences)});
 }
 
 const expect = std.testing.expect;
@@ -53,4 +87,15 @@ test "part 1" {
     var right_items = [_]u32 {4, 3, 5, 3, 9, 3};
 
     try expectEqual(11, computeDistance(&left_items, &right_items));
+}
+
+test "part 2" {
+    var left_items = [_]u32 {3, 4, 2, 1, 3, 3};
+    var right_items = [_]u32 {4, 3, 5, 3, 9, 3};
+
+    var occurrences = std.AutoHashMap(u32, u32).init(std.testing.allocator);
+    defer occurrences.deinit();
+
+    try countOccurrences(&occurrences, &right_items);
+    try expectEqual(31, computeSimilarity(&left_items, &occurrences));
 }
