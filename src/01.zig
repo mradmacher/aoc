003f78e9d8f92c83d8a57a7f44ec1b16ec4c6bca
mradmacher <michal@radmacher.pl>
2024-12-29 11:55:24 +0100

    rename to show it is for day 01

--- src/01.zig
+++ src/01.zig
@@ -70,7 +70,7 @@ pub fn main() !void {
     defer left_list.deinit();
     defer right_list.deinit();
 
-    try loadLists(&left_list, &right_list, "./input");
+    try loadLists(&left_list, &right_list, "./data/01");
 
     std.debug.print("Distance: {d}\n", .{try computeDistance(left_list.items, right_list.items)});
 
